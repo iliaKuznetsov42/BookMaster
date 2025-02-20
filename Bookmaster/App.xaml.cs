15d89becb4171f9c04062829882ab4c7a0efc100
Илья.Кузнецов <.@TERM1>
2025-02-20 10:22:24 +0300

    написан код.

--- Bookmaster/App.xaml.cs
+++ Bookmaster/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Bookmaster.Model;
 using System.Windows;
 
 namespace Bookmaster
@@ -13,5 +8,6 @@ namespace Bookmaster
     /// </summary>
     public partial class App : Application
     {
+        public static BookmasterEntities context = new BookmasterEntities();
     }
 }
